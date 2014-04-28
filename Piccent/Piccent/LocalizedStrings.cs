de639bff2f90c1a1b34b05714f9d420eea06afde
aloisdg <aloisdegouvello@live.fr>
2014-04-28 19:47:10 +0200

    init solution

---
+++ Piccent/Piccent/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Piccent.Resources;
+
+namespace Piccent
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
